32b14e164ee664c59c9809ac751724a571a10f9c
Taiwo Adewole <taiwo.m.adewole1@gmail.com>
2026-01-14 20:17:33 -0500

    Finished set decision action class

--- ruleengine/src/main/java/com/taiade/ruleengine/domain/rule/action/SetDecisionAction.java
+++ ruleengine/src/main/java/com/taiade/ruleengine/domain/rule/action/SetDecisionAction.java
@@ -1,5 +1,24 @@
 package com.taiade.ruleengine.domain.rule.action;
+import com.taiade.ruleengine.domain.rule.*;
+import com.taiade.ruleengine.domain.decision.*;;
 
+/**
+ * SetDecisionAction is something a rule can do when it matches.
+ * Ex: If a rule matches, we want to set the final decision: APPROVE, REJECT, or REVIEW
+ * Sets the decision in the DecisionContext.
+ */
 public class SetDecisionAction {
+    
+    private final Decision decision;
 
+    public SetDecisionAction(Decision decision) {
+        this.decision = decision;
+    }
+
+    public void apply(DecisionContext context){
+        //Sets the decision in the DecisionContext
+        context.setDecision(decision);
+    }
 }
+
+   
